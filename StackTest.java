a8c12a4044953387f57c98e2ca9cbbd8c5bb13f1
tamakiii <tamakiii@users.noreply.github.com>
2023-12-21 16:55:34 +0900

    mv to src/test

--- StackTest.java
+++
@@ -1,9 +0,0 @@
-package stack;
-
-import org.junit.Test;
-
-public class StackTest {
-  @Test
-  public void nothing() throws Exception {
-  }
-}
---
+++ src/test/java/com/example/StackTest.java
@@ -0,0 +1,11 @@
+package stack;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StackTest {
+  @Test
+  public void nothing() throws Exception {
+    assertTrue(true);
+  }
+}
