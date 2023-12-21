b816565aec919d75fd07fc9ab8a2effb59f04ad9
tamakiii <tamakiii@users.noreply.github.com>
2023-12-21 16:52:22 +0900

    install junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,22 @@
  * This is a general purpose Gradle build.
  * To learn more about Gradle by exploring our Samples at https://docs.gradle.org/8.5/samples
  */
+
+plugins {
+    java
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0") // JUnit Jupiter API and Engine
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
