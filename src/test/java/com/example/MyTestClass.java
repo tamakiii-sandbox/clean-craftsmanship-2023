0c194c8a819fac639b540ddf5ff6bfdb7f4cc4f1
tamakiii <tamakiii@users.noreply.github.com>
2023-12-21 16:52:32 +0900

    init test

---
+++ src/test/java/com/example/MyTestClass.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyTestClass {
+    @Test
+    void sampleTest() {
+        assertTrue(true); // Replace with actual tests
+    }
+}
+
